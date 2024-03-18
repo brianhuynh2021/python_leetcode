d5e613098f81f0cc11fde242264a8b597d82737e
brian huynh <huynh2102@gmail.com>
2024-03-18 16:03:03 +0700

    update for interview algorithm one_away

--- 13_one_away.py
+++ 13_one_away.py
@@ -1,35 +1,57 @@
-'''One Away: There are three types of edits that can be performed on strings: insert a character, remove a character, or replace a character. Given two strings, write a function to check if they are one edit (or zero edits) away.
+'''One Away: There are three types of edits that can be performed on strings: 
+insert a character, remove a character, or replace a character. Given two strings, 
+write a function to check if they are one edit (or zero edits) away.
 EXAMPLE
 pale, ple -> true pales, pale -> true pale, bale -> true pale, bake -> false'''
 
 
-def one_away(s1: str, s2: str)-> bool:
-    if abs(len(s1)-len(s2)) > 1:
-        return False
-    if len(s1) == len(s2):
-        dif_count = 0
-        for i in range(len(s1)):
-            if s1[i] == s2[i]:
-                dif_count += 1
-                if dif_count > 1:
-                    return False
-        return True
-    if len(s1) != len(s2):
-        longer, shorter = s1, s2
-    else:
-        longer, shorter = s2, s1
-    
-    i = j = 0
-    diff_count = 0
-    while i < len(longer) and j < len(shorter):
-        if longer[i] != shorter[i]:
-            diff_count += 1
-            if diff_count > 1:
-                return False
-            i += 1
-        else:
-            i += 1
-            j += 1
+def one_away(str1, str2):
+  """
+  Checks if two strings are one edit away (or zero edits).
+
+  Args:
+      str1: The first string.
+      str2: The second string.
+
+  Returns:
+      True if the strings are one edit away, False otherwise.
+  """
+
+  len1, len2 = len(str1), len(str2)
+
+  # Check if the length difference is greater than 1
+  if abs(len1 - len2) > 1:
+    return False
+
+  # If lengths are equal, check for replace operation
+  if len1 == len2:
+    count = 0
+    for i in range(len1):
+      if str1[i] != str2[i]:
+        count += 1
+        if count > 1:
+          return False
     return True
 
-print(one_away('abcd', 'abc'))
+  # If lengths differ by one, check for insert or delete operation
+  longer, shorter = (str1, str2) if len1 > len2 else (str2, str1)
+  i, j = 0, 0
+  while i < len(shorter) and j < len(longer):
+    diff_count = 0
+    if shorter[i] != longer[j]:
+      # check if there is only edit away
+      diff_count += 1
+      if diff_count > 1:
+          return False
+      j += 1
+    
+    else:
+      i += 1
+      j += 1
+  return True
+
+# Test cases
+print(one_away('pale', 'ple'))   # True
+print(one_away('pales', 'pale'))  # True
+print(one_away('pale', 'bale'))  # True
+print(one_away('pale', 'bake'))  # False
