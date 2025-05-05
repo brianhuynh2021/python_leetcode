e54fc87803b030b90953545c569ed1e54c59a24f
Brian <45043707+brianhuynh2021@users.noreply.github.com>
2025-05-05 18:44:06 +0700

    Squaring sorted array + insertion sort here

---
+++ 36_square_sorted_arrr.py
@@ -0,0 +1,29 @@
+'''
+Take a sorted array of integers (may include negative numbers), 
+square each number, and return a new array of the squares, 
+also sorted in non-decreasing order.
+
+Input:  [-4, -1, 0, 3, 10]
+Output: [0, 1, 9, 16, 100]
+'''
+
+def square_array(arr: list)-> list:
+    new_arr = [x*x for x in arr]
+    print(new_arr)
+    return new_arr
+
+# Use insertion sort
+def sorted_array(arr: list)-> list:
+    for i in range(1, len(arr)):
+        compare_element = arr[i]
+        j = i
+        while j > 0 and compare_element < arr[j-1]:
+            arr[j] = arr[j-1]
+            j -= 1
+        arr[j] = compare_element
+    return arr
+
+if __name__=='__main__':
+    arr = [-4, -1, 0, 3, 10]
+    new_arr = square_array(arr)
+    print("Squaring sorted array is: ", sorted_array(new_arr))
