b263eedaca34b8200251d3ddacfe5f18a15e1f7e
Brian <45043707+brianhuynh2021@users.noreply.github.com>
2025-05-13 13:31:12 +0700

    change variable name

--- 40_subarray_product.py
+++ 40_subarray_product.py
@@ -12,20 +12,22 @@
 '''
 from typing import List 
 
-def sub_arrays(nums: List[int], k: int)-> List[List[int]]:
-    sub_arrays = []
+
+def sub_arrays(nums: List[int], k: int)->List[List[int]]:
     n = len(nums)
+    sub_arr = []
     for i in range(n):
         product = 1
         j = i
         for j in range(i, n):
             product *= nums[j]
             if product < k:
-                sub_arrays.append(nums[i:j+1])
+                sub_arr.append(nums[i:j+1])
             else:
                 break
-    return sub_arrays
+    return sub_arr
 
 if __name__ == '__main__':
     nums = [10, 5, 2, 6]
     print('Sub arrays are: ', sub_arrays(nums, 100))
+
