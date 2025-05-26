8464dc7cb2562790d9897858e6cfa01af21a7eb2
Brian <45043707+brianhuynh2021@users.noreply.github.com>
2025-05-26 15:32:44 +0700

    implement more print

--- Coding_Challenge/51_detect_cycle_in_a_linked_list.py
+++ Coding_Challenge/51_detect_cycle_in_a_linked_list.py
@@ -1,50 +1,45 @@
-'''
+"""
 🔁 Chu trình (Cycle) là gì?
 
-Giả sử ta không có node nào trỏ đến NULL, mà một node nào đó lại trỏ ngược 
-về node trước nó hoặc một node nào đó trước đó ⇒ Danh sách sẽ 
+Giả sử ta không có node nào trỏ đến NULL, mà một node nào đó lại trỏ ngược
+về node trước nó hoặc một node nào đó trước đó ⇒ Danh sách sẽ
 bị lặp vô hạn ⇒ gọi là có chu trình.
 
 Ví dụ có cycle:
-'''
+"""
+
 
 class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
-        
+
+
 def has_cycle(head):
     slow = head
     fast = head
-    
+    step = 0
     while fast and fast.next:
         slow = slow.next
         fast = fast.next.next
-        
+        step += 1
+        print(f"[Step {step}] slow at {slow.value}, fast at {fast.value}")
         if slow == fast:
+            print(f"Gap nhau tai Node co gia tri: {slow.value}")
             return True
+    print("==> Khong co chu trinh")
     return False
 
-# 🧪 Test Case 1: Không có chu trình
 a = Node(1)
 b = Node(2)
 c = Node(3)
 d = Node(4)
+e = Node(5)
 
 a.next = b
 b.next = c
 c.next = d
-# d.next = None
+d.next = e
+e.next = c
 
-print("Test 1:", "Có chu trình" if has_cycle(a) else "Không có chu trình")
-
-# 🧪 Test Case 2: Có chu trình
-x = Node(10)
-y = Node(20)
-z = Node(30)
-
-x.next = y
-y.next = z
-z.next = y  # Tạo chu trình
-
-print("Test 2:", "Có chu trình" if has_cycle(x) else "Không có chu trình")
+has_cycle(a)
