8b0a8759edabd8efd2e75df80ec925811996ea14
Brian <45043707+brianhuynh2021@users.noreply.github.com>
2025-07-02 23:51:39 +0700

    agnostic binary search

---
+++ Coding_Challenge/73_order_agnostic_binary_search.py
@@ -0,0 +1,32 @@
+from typing import List
+
+
+def order_agnostic_binary_search(arr: List[int], target: int)->bool:
+    if not arr:
+        return False
+    n = len(arr)
+    is_ascending = arr[0] < arr[-1]
+    low = 0
+    high = n - 1
+    while low <= high:
+        mid = (low + high)//2
+        if arr[mid] == target:
+            return True
+        if is_ascending:
+            if arr[mid] < target:
+               low = mid + 1
+            else:
+                high = mid - 1
+        else:
+            if arr[mid] < target:
+                high = mid - 1
+            else:
+                low = mid + 1
+    return False
+
+if __name__=='__main__':
+    print(order_agnostic_binary_search([1, 3, 5, 7, 9], 7))     # True
+    print(order_agnostic_binary_search([9, 7, 5, 3, 1], 7))     # True
+    print(order_agnostic_binary_search([], 1))                  # False
+    print(order_agnostic_binary_search([42], 42))              # True
+    print(order_agnostic_binary_search([5, 3, 1], 0))           # False
