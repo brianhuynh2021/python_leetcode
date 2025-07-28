b582e11fb7f064093db72b21305399a18a4722d8
Brian <45043707+brianhuynh2021@users.noreply.github.com>
2025-07-28 13:18:42 +0700

    redo problem detect cycle in linkedlist

---
+++ coding_challenge/Python/lc_51_detect_cycle_in_a_linked_list_2nd.py
@@ -0,0 +1,33 @@
+"""
+🔁 Chu trình (Cycle) là gì?
+
+Giả sử ta không có node nào trỏ đến NULL, mà một node nào đó lại trỏ ngược
+về node trước nó hoặc một node nào đó trước đó ⇒ Danh sách sẽ
+bị lặp vô hạn ⇒ gọi là có chu trình.
+
+Ví dụ có cycle:
+"""
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+def detect_cycle_brute(head: Node) -> bool:
+    """
+    Detects if there is a cycle in the linked list using a set to track visited nodes.
+
+    Args:
+        head (Node): Head of the singly linked list.
+
+    Returns:
+        bool: True if cycle exists, False otherwise.
+    """
+    visited_nodes = set()
+    curr_node = head
+    while curr_node:
+        if curr_node in visited_nodes:
+            return True # Cycle detected
+        visited_nodes.add(curr_node)
+        curr_node = curr_node.next
+    return False
