559a0a7bca4818af618e68fd0bf07124432826c0
Brian <45043707+brianhuynh2021@users.noreply.github.com>
2025-10-15 17:52:01 +0700

    update unittest for lc_101_search in BST

--- coding_challenge/Python/tests/test_lc_101_search_in_BST.py
+++ coding_challenge/Python/tests/test_lc_101_search_in_BST.py
@@ -8,19 +8,22 @@ class TestSearchBST(unittest.TestCase):
         self.root.left = TreeNode(2, TreeNode(1), TreeNode(3))
         self.root.right = TreeNode(7)
         self.solution = Solution()
-        
+
     def test_find_existing_node_recursive(self):
         """Test finding an existing node using the recursive method"""
         # Search for value 2
         result_node = self.solution.search_BST(self.root, 2)
-        self.assertIsNone(result_node)
+        self.assertIsNotNone(result_node)
         self.assertEqual(result_node.val, 2)
         # Check if it's the correct subtree
         self.assertEqual(result_node.left.val, 1)
         self.assertEqual(result_node.right.val, 3)
-        
-        
+
     def test_find_non_existing_node_recursive(self):
-        """Test searching for a non-existing node """
+        """Test searching for a non-existing node"""
         result_node = self.solution.search_BST_interative(self.root, 5)
         self.assertIsNone(result_node)
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
