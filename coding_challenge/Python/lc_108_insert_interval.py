f300b20cf84e26d877e4d71ac484202d6c9e7ba9
Brian <45043707+brianhuynh2021@users.noreply.github.com>
2026-05-06 00:28:43 +0700

    optimized insert intervals

--- coding_challenge/Python/lc_108_insert_interval.py
+++ coding_challenge/Python/lc_108_insert_interval.py
@@ -1,14 +1,14 @@
 """
-Bài toán / Problem
-Bạn có một danh sách các khoảng thời gian đã được sắp xếp theo start và không overlap.
-You are given a list of non-overlapping intervals sorted by start time.
-ex:
-intervals = [[1, 3], [6, 9]]
-new_interval = [2, 5]
+    Bài toán / Problem
+    Bạn có một danh sách các khoảng thời gian đã được sắp xếp theo start và không overlap.
+    You are given a list of non-overlapping intervals sorted by start time.
+    ex:
+    intervals = [[1, 3], [6, 9]]
+    new_interval = [2, 5]
 """
 
 
-def insert_interval(intervals: list, new_interval: list) -> list:
+def insert_interval_brute_force(intervals: list, new_interval: list) -> list:
     intervals = intervals + [new_interval]
     intervals = sorted(intervals, key=lambda x: x[0])
     result = []
@@ -18,4 +18,28 @@ def insert_interval(intervals: list, new_interval: list) -> list:
             result.append(current)
         else:
             result[-1][1] = max(result[-1][1], current[1])
+    return result
+
+def insert_interval_optimized(intervals: list, new_interval) -> list:
+    result = []
+    i = 0
+    n = len(intervals)
+    
+    # 1. Add intervals before new_interval
+    while i < n and intervals[i][1] < new_interval[0]:
+        result.append(intervals[i])
+        i += 1
+    
+    
+    # merge overlap intervals
+    while i < n and intervals[i][0] <= new_interval[1]:
+        new_interval[0] = min(new_interval[0], intervals[i][0])
+        new_interval[1] = max(new_interval[1], intervals[i][1])
+        i += 1
+        
+    result.append(new_interval)
+    while i < n:
+        result.append(intervals[i])
+        i+= 1
+        
     return result
